b2dc9555d16ae444896a9cbcaf64a79ffd7c0648
AnthonyUPF <anthony.torres.h@gmail.com>
2023-04-28 13:58:51 +0200

    changes PostMapping

--- BackEnd/src/main/java/backapp/demo/controllers/SongController.java
+++ BackEnd/src/main/java/backapp/demo/controllers/SongController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import backapp.demo.models.Support;
 import backapp.demo.repositories.DataRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class SongController {
     @Autowired
