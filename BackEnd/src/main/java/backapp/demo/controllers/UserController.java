5f7f46543818359f16a716b6091496a48efbcb93
Natalie <ngomez.cep@gmail.com>
2023-04-28 13:34:26 +0200

    Back conectado con Front

--- BackEnd/src/main/java/backapp/demo/controllers/UserController.java
+++ BackEnd/src/main/java/backapp/demo/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,8 @@ import backapp.demo.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
+
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -36,7 +39,7 @@ public class UserController {
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The user already exists in the repository");
         } else {
-            return userRepository.save(user);
+            return userRepository.saveAndFlush(user);
         }
     }
     
