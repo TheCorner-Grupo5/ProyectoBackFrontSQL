cce0d15be84241c31c39fbe0a7ed02eef0c0df5a
AnthonyUPF <anthony.torres.h@gmail.com>
2023-05-01 23:12:05 +0200

    old changes

--- FrontEnd/js/SongPut.js
+++ FrontEnd/js/SongPut.js
@@ -5,21 +5,29 @@ form.addEventListener('submit', function(event) {
 
 	const user = {
 		id: form.elements.id.value,
+		firstName: form.elements.firstName.value,
+		lastName: form.elements.lastName.value,
+		email: form.elements.email.value,
+		avatar: form.elements.avatar.value
 	};
 
-	fetch('http://localhost:8080/api/deleteSong'+'?id='+user.id, {
-		method: 'DELETE'
+	fetch('http://localhost:8080/api/updateSong', {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(user)
 	})
 	.then(response => response.json())
 	.then(data => {
 		console.log('Success:', data);
-		
+
 		// Reset the form
 		form.reset();
 
 		// Refresh the page after a delay of 1 second
 		setTimeout(() => {
-		  location.reload();
+			location.reload();
 		});
 	})
 	.catch((error) => {
