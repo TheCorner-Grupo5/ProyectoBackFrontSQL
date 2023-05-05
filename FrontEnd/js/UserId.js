00861689bdfe8bdff45942cc341afa3b66ec02a2
AnthonyUPF <anthony.torres.h@gmail.com>
2023-05-05 09:36:14 +0200

    SongId and UserId

---
+++ FrontEnd/js/UserId.js
@@ -0,0 +1,15 @@
+const idSelect = document.getElementById('id');
+
+fetch('http://localhost:8080/api/getUsers')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(item => {
+      const option = document.createElement('option');
+      option.value = item.id;
+      option.textContent = item.id;
+      idSelect.appendChild(option);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
--- FrontEnd/js/UserPost.js
+++ FrontEnd/js/UserPost.js
@@ -20,6 +20,14 @@ form.addEventListener('submit', function(event) {
 	.then(response => response.json())
 	.then(data => {
 		console.log('Success:', data);
+
+		// Reset the form
+		form.reset();
+
+		// Refresh the page after a delay of 1 second
+		setTimeout(() => {
+			location.reload();
+		});
 	})
 	.catch((error) => {
 		console.error('Error:', error);
